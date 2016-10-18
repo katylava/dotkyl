8abe29a5450c2b51ee92c3577025298d0926336d
katy lavallee <katylava@gmail.com>
2016-10-18 10:01:35 -0500

    Update promptline config

---
+++ lib/085-virtualenv.zsh
@@ -0,0 +1,8 @@
+# look for a parent folder with .venv
+#     if found, source it
+#     if not found, and deactivate is defined, then deactivate
+
+# function findvenv() {
+# }
+#
+# add to precmd_functions
