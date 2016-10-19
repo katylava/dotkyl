2d38967570304a1f5244796b42cf589808535cbf
katy lavallee <katylava@gmail.com>
2016-10-19 10:20:24 -0500

    Add zsh-autosuggestions

--- lib/020-keybindings.zsh
+++ lib/020-keybindings.zsh
@@ -2,3 +2,4 @@ bindkey -e
 bindkey -M menuselect '^o' accept-and-infer-next-history
 bindkey '\e[A' history-beginning-search-backward
 bindkey '\e[B' history-beginning-search-forward
+bindkey '^J' autosuggest-accept
