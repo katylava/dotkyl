52dd75b408e7f038880072578ba297335db15a29
katy lavallee <katylava@gmail.com>
2017-04-27 10:20:04 -0500

    keybindings: Add URLs for when i change to bindkey -v

--- lib/020-keybindings.zsh
+++ lib/020-keybindings.zsh
@@ -1,3 +1,7 @@
+# TODO: add stuff from https://github.com/robbyrussell/oh-my-zsh/blob/master/plugins/vi-mode/vi-mode.plugin.zsh
+# ...more good stuff here: https://unix.stackexchange.com/a/344028
+# ...and easier escape: https://superuser.com/a/353127
+# http://pawelgoscicki.com/archives/2012/09/vi-mode-indicator-in-zsh-prompt/
 bindkey -e
 bindkey -M menuselect '^o' accept-and-infer-next-history
 bindkey '\e[A' history-beginning-search-backward
