4d05faa8d7e64a4cf02fd73af78dfeaca6fa7edb
katy lavallee <katylava@gmail.com>
2017-05-30 14:59:46 -0500

    zsh: Remove fzf source from lib
    ...it puts itself back in ~/.zshrc

--- lib/100-installed.zsh
+++ lib/100-installed.zsh
@@ -13,8 +13,6 @@ export HH_CONFIG=hicolor
 
 test -e "${HOME}/.iterm2_shell_integration.zsh" && source "${HOME}/.iterm2_shell_integration.zsh"
 
-[ -f ~/.fzf.zsh ] && source ~/.fzf.zsh
-
 source /usr/local/share/zsh-autosuggestions/zsh-autosuggestions.zsh
 source /usr/local/share/zsh-syntax-highlighting/zsh-syntax-highlighting.zsh
 
