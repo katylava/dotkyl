ec36c2ad2a6798de1c02d25101f947bd6b406942
katy lavallee <katylava@gmail.com>
2017-08-22 08:47:21 -0500

    zsh: Add nvm

--- lib/100-installed.zsh
+++ lib/100-installed.zsh
@@ -35,3 +35,6 @@ fuck() {
 eval "$(pyenv init -)"
 # This is not worth the startup time
 # [[ $(docker-machine status default) != 'Stopped' ]] && eval "$(docker-machine env default)"
+
+export NVM_DIR="$HOME/.nvm"
+. "/usr/local/opt/nvm/nvm.sh"
