8c1008c209439030632c43a524ce62b86491341a
katy lavallee <katylava@gmail.com>
2017-11-30 18:24:13 -0600

    lib: Manage iterm2 settings in dotfiles

---
+++ lib/101-iterm.zsh
@@ -0,0 +1,21 @@
+# alter iterm settings based on things and stuff
+
+
+__danger_branch() {
+    [ -z ${(t)DANGER_BRANCHES} ] && return 0
+
+    local branch=`git_current_branch`
+    return ${DANGER_BRANCHES[(I)${branch}]}
+}
+
+__set_iterm() {
+    __danger_branch
+
+    if [ $? -eq 0 ]; then
+        printf "\e]1337;SetBadgeFormat=\a"
+    else
+        printf "\e]1337;SetBadgeFormat=%s\a" $(echo -n "🚫" | base64)
+    fi
+}
+
+precmd_functions+=(__set_iterm)
