07ac633f50bb58cf42f03459ecdfe5046a740bfb
katy lavallee <katylava@gmail.com>
2018-01-04 09:15:18 -0600

    zsh: Add precmd to set node/python version

--- lib/085-virtualenv.zsh
+++
@@ -1,8 +0,0 @@
-# look for a parent folder with .venv
-#     if found, source it
-#     if not found, and deactivate is defined, then deactivate
-
-# function findvenv() {
-# }
-#
-# add to precmd_functions
--- lib/999-autoenv.zsh
+++
@@ -1,4 +0,0 @@
-# I don't use autoenv enough to make this worth the startup time
-
-# must be last
-# source $(brew --prefix autoenv)/activate.sh
