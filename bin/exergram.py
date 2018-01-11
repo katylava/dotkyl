4b0de8db59691c99dd1fa0f3ec686a39965cda95
katy lavallee <katylava@gmail.com>
2018-01-11 16:48:45 -0600

    bin: Add hidden,symmetry support to exergram

--- bin/exergram.py
+++ bin/exergram.py
@@ -8,24 +8,33 @@ import sys
 from collections import namedtuple
 
 
-with open(os.path.expanduser('~/Dropbox/exercises/exercises.json')) as fh:
+EXERCISE_FILE = os.path.expanduser('~/Dropbox/exercises/exercises.json')
+
+if not os.path.exists(EXERCISE_FILE):
+    print('No exercises.json found at ~/Dropbox/exercises/')
+    sys.exit(1)
+
+with open(EXERCISE_FILE) as fh:
     _EXERCISES = json.load(fh)
 
-Exercise = namedtuple('Exercise', ['name', 'effort', 'kind', 'equipment'])
+Exercise = namedtuple('Exercise', [
+    'name', 'effort', 'kind', 'equipment', 'hidden', 'symmetric'
+])
 EXERCISES = [Exercise(**x) for x in _EXERCISES]
+EFFORT_LEVELS = ['easy', 'medium', 'hard', 'very hard']
 
 
 def select(num, kind=None, effort=None, equipment=None):
     """ Select random `num` exercises matching kwarg filters """
-    exercises = EXERCISES
+    exercises = [x for x in EXERCISES if not x.hidden]
 
-    equipment = equipment + [''] if equipment else ['']
+    equipment = equipment + [None] if equipment else [None]
 
     if kind and kind != ['any']:
         exercises = [x for x in exercises if x.kind in kind]
     if effort:
         exercises = [x for x in exercises if x.effort <= effort]
-    if equipment:
+    if equipment and equipment != ['any']:
         exercises = [x for x in exercises if x.equipment in equipment]
 
     num = len(exercises) if num > len(exercises) else num
@@ -54,50 +63,56 @@ def collate(exercises):
                 rest.remove(x)
                 return [x, rest]
         rest.remove(x)
-        return [x, rest]
+        return (x, rest)
 
-    sorted = []
+    collated = []
     current = None
 
     while len(exercises):
         next, exercises = getnext(exercises, current)
-        sorted.append(next)
+        collated.append(next)
         current = next
 
-    return sorted
+    return collated
 
 
-if __name__ == '__main__':
-    num = int(sys.argv[1])
-    effort = int(sys.argv[2])
-    kind = sys.argv[3].split(',') if len(sys.argv) > 3 else None
-    equipment = sys.argv[4].split(',') if len(sys.argv) > 4 else None
+def print_exercises(exercises):
+    """ Output exercise info to stdout """
+    tpl = (
+        '{name:<{name_width}}{kind:<{kind_width}}{effort:<{effort_width}}'
+        '{equipment:<{equipment_width}}{asymmetric}'
+    )
 
-    exercises = collate(select(num, kind, effort, equipment))
-
-    longest_name = max([len(x.name) for x in exercises])
-    longest_kind = max([len(x.kind) for x in exercises])
-    tpl = '{name:<{name_width}} {kind:<{kind_width}} {effort:<13} {equipment}'
+    name_width = max([len(x.name) for x in exercises]) + 4
+    kind_width = max([len(x.kind) for x in exercises]) + 4
+    effort_width = max([len(effort) for effort in EFFORT_LEVELS]) + 4
+    equipment_width = max([len(x.equipment or '') for x in exercises]) + 4
 
     print(tpl.format(
         name='Name',
-        name_width=longest_name + 4,
         kind='Kind',
-        kind_width=longest_kind + 4,
         effort='Effort',
-        equipment='Equipment',
+        equipment='Eq.',
+        asymmetric='',
+        name_width=name_width,
+        kind_width=kind_width,
+        effort_width=effort_width,
+        equipment_width=equipment_width,
     ))
 
-    print('━' * (longest_name + 4 + 1 + longest_kind + 4 + 1 + 13 + 1 + 9))
+    print('━' * (name_width + kind_width + effort_width + equipment_width + 4))
 
     for x in exercises:
         print(tpl.format(
             name=x.name,
-            name_width=longest_name + 4,
             kind=x.kind,
-            kind_width=longest_kind + 4,
-            effort=['easy', 'medium', 'hard', 'very hard'][x.effort - 1],
+            effort=EFFORT_LEVELS[x.effort - 1],
             equipment=x.equipment or '--',
+            asymmetric='  ⨉2' if not x.symmetric else '',
+            name_width=name_width,
+            kind_width=kind_width,
+            effort_width=effort_width,
+            equipment_width=equipment_width,
         ))
 
     print('\n\nImages\n━━━━━━')
@@ -110,6 +125,45 @@ if __name__ == '__main__':
 
         if os.path.exists(image_path):
             print('{}: {}'.format(x.name, image_path))
+
+
+def print_usage():
+    kinds = list(set([x.kind for x in EXERCISES]))
+    equipments = list(set([x.equipment for x in EXERCISES if x.equipment]))
+    print("""
+        exergram.py [num exercises] <max effort> <kind> <equipment>
+
+        Reads a list of exercises from a json file at
+        ~/Dropbox/exercises/exercises.json and selects `num exercises`
+        randomly, collates them by effort and kind, and outputs list
+        to stdout.
+
+        num exercises:  the number of exercises you want to output.
+        max effort:     a number 1-4 with 1 being easy and 4 being very hard
+                        -- default 4
+        kind:           a comma-separated list to filter exercises by kind. The
+                        special value "any" will match all exercises.
+                        Options: {kinds}
+        equipment:      a comma-separated list of available equipment. By
+                        default only exercises which require no equipment are
+                        selected.  Specifying equipment will include exercises
+                        which require that equipment as well as any which
+                        require none.
+                        Options: {equipments}
+    """.format(kinds=', '.join(kinds), equipments=', '.join(equipments)))
+
+
+if __name__ == '__main__':
+    if len(sys.argv) <= 2:
+        print_usage()
+        sys.exit()
+
+    num = int(sys.argv[1])
+    effort = int(sys.argv[2])
+    kind = sys.argv[3].split(',') if len(sys.argv) > 3 else None
+    equipment = sys.argv[4].split(',') if len(sys.argv) > 4 else None
+
+    print_exercises(collate(select(num, kind, effort, equipment)))
 
 # Copyright (c) 2018 Kathleen LaVallee <katylava@gmail.com>
 
