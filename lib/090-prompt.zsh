796b0a5551aff8316bf353479404c837b2b798af
katy lavallee <katylava@gmail.com>
2020-01-24 17:34:19 -0600

    zsh: Show bookmark names in prompt

--- lib/090-prompt.zsh
+++ lib/090-prompt.zsh
@@ -1,6 +1,9 @@
 #
-# This shell prompt config file was created by promptline.vim
+# This shell prompt config file was created by promptline.vim, then very
+# heavily modified.
 #
+
+NEWLINE=$'\n'
 
 function __promptline_symbol {
     local symbol="✰"
@@ -89,38 +92,48 @@ function __promptline_vcs_branch {
   return 1
 }
 function __promptline_cwd {
-  local dir_limit="3"
+  local dir_limit="2"
   local truncation="⋯"
-  local first_char
+  local trunc_part=""
   local part_count=0
   local formatted_cwd=""
   local dir_sep="  "
   local tilde="~"
 
-  local cwd="${PWD/#$HOME/$tilde}"
+  # this file expansion syntax will give us a path that uses bookmark names
+  local first_dir="${(%):-%-1~}"
+  local last_dirs="${(%):-%3~}"
 
-  # get first char of the path, i.e. tilde or slash
-  [[ -n ${ZSH_VERSION-} ]] && first_char=$cwd[1,1] || first_char=${cwd::1}
+  # if the first directory is right under root, then let's just use root as the
+  # first directory instead
+  [[ "$first_dir[1]" == "/" ]] && first_dir="/"
 
-  # remove leading tilde
-  cwd="${cwd#\~}"
-
-  while [[ "$cwd" == */* && "$cwd" != "/" ]]; do
-    # pop off last part of cwd
-    local part="${cwd##*/}"
-    cwd="${cwd%/*}"
-
+  # For each directory in the last 3 directories...
+  for part in ${(s:/:)${last_dirs}}; do
+    # ... make sure it's not a dupe of the first director, which happens then
+    # there are 3 or fewer directories in the path
+    [[ $part == $first_dir ]] && continue
+    # then truncate really long directory names
     len="${#part}"
-
     [[ len -gt 15 ]] && part=${part:0:7}⁎${part:$len-7:7}
-
-    formatted_cwd="$dir_sep$part$formatted_cwd"
-    part_count=$((part_count+1))
-
-    [[ $part_count -eq $dir_limit ]] && first_char="$truncation" && break
+    # and append this part to the string we are building up for the prompt
+    formatted_cwd="$formatted_cwd$dir_sep$part"
   done
 
-  printf "%s" "$first_char$formatted_cwd"
+  # For directories under ~ or a bookmark, include the truncation string if
+  # there are more directories between first_dir and last_dirs. The first
+  # condition here handles an edge case with being at the root of a bookmarked
+  # directory, where the second condition would get the number of chars in the
+  # bookmark name instead of the number of directories in the path.
+  [[ $part != $first_dir ]] && [[ ${#${(s:/:)${(%):-%~}}} -gt 4 ]] \
+      && trunc_part=$dir_sep$truncation
+
+  # For directories NOT under ~ or a bookmark, we pulled the real first
+  # directory off of it, so we need to truncate at 3 instead of 4.
+  [[ $first_dir == "/" ]] && [[ ${#${(s:/:)${(%):-%~}}} -gt 3 ]] \
+      && trunc_part=$dir_sep$truncation
+
+  printf "%s" "$first_dir$trunc_part$formatted_cwd"
 }
 function __promptline_left_prompt {
   local slice_prefix slice_empty_prefix slice_joiner slice_suffix is_prompt_empty=1
@@ -278,7 +291,7 @@ function __promptline {
   local z_bg="${wrap}43${end_wrap}"
   local z_sep_fg="${wrap}33${end_wrap}"
   if [[ -n ${ZSH_VERSION-} ]]; then
-    PROMPT="$(__promptline_left_prompt)"
+    PROMPT="$(__promptline_left_prompt)$NEWLINE➯ "
     RPROMPT="$(__promptline_right_prompt)"
   elif [[ -n ${FISH_VERSION-} ]]; then
     if [[ -n "$1" ]]; then
