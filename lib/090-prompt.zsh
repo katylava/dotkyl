eb69570d4c4dc80291af8e7470f9968201c83f01
katy lavallee <katylava@gmail.com>
2020-09-25 11:57:54 -0500

    zsh: Change prompt symbol

--- lib/090-prompt.zsh
+++ lib/090-prompt.zsh
@@ -12,7 +12,7 @@ function __promptline_symbol {
     # (under Profile > Text) seems to fix any weirdness
     # with using an emoji in the prompt
     # ... never mind, no it doesn't
-    local symbol='⛵'
+    local symbol='🍂'
 
     mail -e
     [ $? -eq 0 ] && symbol="📨"
