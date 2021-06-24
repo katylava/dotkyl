4509dd18f209f99b3770f4ce4d8ac7f9c0302273
katy lavallee <katylava@gmail.com>
2021-06-24 11:29:09 -0500

    zsh: Add "browse" alias
    To view current repo on github. Aliases to `gh repo view --web`.

--- lib/010-aliases.zsh
+++ lib/010-aliases.zsh
@@ -34,6 +34,7 @@ alias rpass='</dev/urandom tr -dc A-Za-z0-9 | head -c 10'
 alias updatedb="export LC_ALL='C' && sudo gupdatedb"
 
 # things i just hate typing
+alias browse='gh repo view --web'
 alias dc='docker-compose'
 alias dice='rolldice -s'
 alias edocker='eval "$(docker-machine env default)"'
