cefa5e95cbf0ac4713640f24fdef9201dd258a29
katy lavallee <katylava@gmail.com>
2024-01-07 09:58:32 -0600

    zsh: Remove timetrace starship integration

--- bin/ttwarning.zsh
+++
@@ -1,9 +0,0 @@
-#! /usr/bin/env zsh
-
-PROJECT=$(timetrace status -f '{project}')
-
-test "$PWD" != "/Users/kyl/Code/pct" && exit 1
-
-[[ "${PROJECT}" = '---' || "${PROJECT}" =~ 'today' ]] && exit 0
-
-exit 1
