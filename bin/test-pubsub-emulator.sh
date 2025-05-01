8c745692c6ab10ab509e540d42d1d455bedf8f92
katy lavallee <klavallee@oreilly.com>
2025-05-01 11:30:14 -0500

    bin: Add script to test pubsub emulator connection

--- bin/test-pubsub-emulator.sh
+++ bin/test-pubsub-emulator.sh
@@ -2,10 +2,18 @@
 
 # After starting the emulator with `docker compose up pubsub-emulator` you can
 # run this script from the host to ensure the emulator is working as expected.
+#
+# Pass any argument to just list topics and subscriptions without creating a
+# new topic.
 
 PROJECT=local-project
 PUBSUB_EMULATOR_HOST=0.0.0.0:8085
-TOPIC=test-connectivity-$(head -c 32 /dev/urandom | base64 | tr -dc a-zA-Z0-9 | head -c 8)-topic
 
-curl -X PUT -H "Content-Type: application/json" http://${PUBSUB_EMULATOR_HOST}/v1/projects/${PROJECT}/topics/${TOPIC}
+if [[ -z $1 ]]; then
+  TOPIC=test-connectivity-$(head -c 32 /dev/urandom | base64 | tr -dc a-zA-Z0-9 | head -c 8)-topic
+  echo "Creating topic ${TOPIC}"
+  curl -X PUT -H "Content-Type: application/json" http://${PUBSUB_EMULATOR_HOST}/v1/projects/${PROJECT}/topics/${TOPIC}
+fi
+
 curl -X GET -H "Content-Type: application/json" http://${PUBSUB_EMULATOR_HOST}/v1/projects/${PROJECT}/topics
+curl -X GET -H "Content-Type: application/json" http://${PUBSUB_EMULATOR_HOST}/v1/projects/${PROJECT}/subscriptions
